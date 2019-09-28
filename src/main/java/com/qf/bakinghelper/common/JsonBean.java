7391e341974d4eca49e35bc958b1f0e2d1fd9174
Hoan <1823427924@qq.com>
2019-09-28 17:48:35 +0800

    //加入注释

--- src/main/java/com/qf/bakinghelper/common/JsonBean.java
+++ src/main/java/com/qf/bakinghelper/common/JsonBean.java
@@ -3,15 +3,15 @@ package com.qf.bakinghelper.common;
 import lombok.Data;
 
 @Data
-public class JsonBean {
+public class JsonBean<T> {
 
-	private int code;
-	private Object info;
+	private int code;//1:代表成功
+	private T info;
 
 	public JsonBean() {
 	}
 
-	public JsonBean(int code, Object info) {
+	public JsonBean(int code, T info) {
 		this.code = code;
 		this.info = info;
 	}
