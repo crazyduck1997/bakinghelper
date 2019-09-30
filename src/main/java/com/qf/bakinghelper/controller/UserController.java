f242c13e8d82a15fd5a78f123e507c1b06df6cb1
Hoan <1823427924@qq.com>
2019-09-30 16:35:50 +0800

    //添加修改密码，注销，手机号登录

--- src/main/java/com/qf/bakinghelper/controller/UserController.java
+++ src/main/java/com/qf/bakinghelper/controller/UserController.java
@@ -7,10 +7,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
 
-import javax.servlet.http.HttpSession;
 
 @Api(value = "用户")
 @RestController
@@ -27,26 +26,38 @@ public class UserController {
         return new JsonBean(1,token);
     }
 
-    @ApiOperation(value = "校验验证码")
+    @ApiOperation(value = "校验验证码,完成注册")
     @PostMapping("/verifyCode.do")
-    public JsonBean verifyCode(String code,String token){
-        userService.verifyCode(code, token);
-        System.out.println(token);
-        return new JsonBean(1,token);
+    public JsonBean verifyCode(String code,String token,String password){
+        User user = userService.regist(code, token, password);
+        return new JsonBean(1,user);
     }
 
-    @ApiOperation(value = "获取密码完成注册")
-    @PostMapping("/regist.do")
-    public JsonBean regist(String password, String token){
-        User user = userService.regist(password, token);
-        return new JsonBean(1,"成功");
-    }
 
     @ApiOperation(value = "手机号密码登录")
     @PostMapping("/login.do")
     public JsonBean login(String phone,String password){
-        User user = userService.login(phone,password);
+        String token = userService.login(phone, password);
+        return new JsonBean(1,token);
+    }
+
+
+    @ApiOperation(value = "用户注销")
+    @PostMapping("/loginOut.do")
+    public JsonBean loginOut(String token){
+        String loginOut = userService.loginOut(token);
+        return new JsonBean(1,loginOut);
+    }
+
+    @ApiOperation(value = "个人信息展示以及账户信息展示")
+    @PostMapping("/userInfo")
+    public JsonBean userInfo(String token){
+        User user = userService.userInfo(token);
         return new JsonBean(1,user);
     }
+
+
+
+
 
 }
