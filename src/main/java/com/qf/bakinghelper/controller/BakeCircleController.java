cd60b538ba78e1f766cc97b1ba7046ecd10d86a2
myl <784470242@qq.com>
2019-10-03 10:00:58 +0800

    添加跨域

--- src/main/java/com/qf/bakinghelper/controller/BakeCircleController.java
+++ src/main/java/com/qf/bakinghelper/controller/BakeCircleController.java
@@ -6,6 +6,7 @@ import com.qf.bakinghelper.service.BakeCircleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
 
 
 @Api(value = "烘焙圈")
+@CrossOrigin
 @RestController
 @RequestMapping("/bakecircle")
 public class BakeCircleController {
