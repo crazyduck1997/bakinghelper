19c5b8bb3f7d79466f4532c2683f01105fc96137
Hoan <1823427924@qq.com>
2019-10-03 10:00:57 +0800

    //修改注册

--- src/main/java/com/qf/bakinghelper/controller/UserController.java
+++ src/main/java/com/qf/bakinghelper/controller/UserController.java
@@ -6,11 +6,13 @@ import com.qf.bakinghelper.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Api(value = "用户")
+@CrossOrigin
 @RestController
 public class UserController {
 
