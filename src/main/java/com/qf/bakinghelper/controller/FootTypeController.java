1b50d54d950d4959b564b999cd88424710b530e4
gfq <335314139@qq.com>
2019-10-04 15:32:31 +0800

    //食谱分类中根据具体的食品查询视频

--- src/main/java/com/qf/bakinghelper/controller/FootTypeController.java
+++ src/main/java/com/qf/bakinghelper/controller/FootTypeController.java
@@ -2,6 +2,7 @@ package com.qf.bakinghelper.controller;
 
 import com.qf.bakinghelper.common.JsonBean;
 import com.qf.bakinghelper.entity.FootType;
+import com.qf.bakinghelper.entity.Video;
 import com.qf.bakinghelper.service.FootTypeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,5 +26,20 @@ public class FootTypeController {
     public JsonBean<List<FootType>> footTypeList(){
         List<FootType> footTypes = footTypeService.footTypeList();
             return new JsonBean(1,footTypes);
+    }
+
+    @ApiOperation(value = "食谱分类中根据类型查找名字中含有此类型的视频")
+    @PostMapping("/findVideo")
+    public JsonBean<List<Video>> findVideo(String typeName){
+        List<Video> videos = footTypeService.findFootType(typeName);
+        return new JsonBean<>(1,videos);
+    }
+
+
+    @ApiOperation(value = "查询食谱分类中的推荐")
+    @PostMapping("/footList")
+    public JsonBean<List<FootType>> footList(){
+        List<FootType> footTypes = footTypeService.footList();
+        return new JsonBean<>(1,footTypes);
     }
 }
--- src/main/java/com/qf/bakinghelper/service/impl/FootTypeServiceImpl.java
+++ src/main/java/com/qf/bakinghelper/service/impl/FootTypeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.qf.bakinghelper.service.impl;
 
 import com.qf.bakinghelper.dao.FootTypeDao;
+import com.qf.bakinghelper.dao.VideoDao;
 import com.qf.bakinghelper.entity.FootType;
+import com.qf.bakinghelper.entity.Video;
 import com.qf.bakinghelper.service.FootTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,25 @@ import java.util.List;
 public class FootTypeServiceImpl implements FootTypeService {
 
 
-    @Autowired
+    @Autowired(required = false)
     FootTypeDao footTypeDao;
+
+    @Autowired(required = false)
+    VideoDao videoDao;
     @Override
     //    根据具体的食品类型查询所有的食品
     public List<FootType> footTypeList() {
         return footTypeDao.footTypeList();
+    }
+
+    @Override
+//    根据具体的食品查询视频
+    public List<Video> findFootType(String typeName) {
+        return videoDao.findByFootType(typeName);
+    }
+
+    @Override
+    public List<FootType> footList() {
+        return footTypeDao.footList();
     }
 }
