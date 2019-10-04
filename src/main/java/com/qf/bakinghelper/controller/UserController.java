24de5cf55e86eb8f3c3519f39bf18b948ce2e24f
Hoan <1823427924@qq.com>
2019-10-04 08:53:02 +0800

    //userController添加一级目录

--- src/main/java/com/qf/bakinghelper/controller/UserController.java
+++ src/main/java/com/qf/bakinghelper/controller/UserController.java
@@ -8,13 +8,14 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.spring.web.json.Json;
 
 
 @Api(value = "用户")
 @CrossOrigin
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
